e91bb2cbaa1aeb7b5779358cef7ec3bda7a8c78a
Babsia <63377866+Babsia@users.noreply.github.com>
2023-05-13 19:01:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def retorno(pelicula):
     inversion = df_pelicula['budget'].sum()
     ganancia = df_pelicula['revenue'].sum()-inversion
     retorno = df_pelicula['return'].sum()
-    anio = df_pelicula['release_year'].sum()
+    anio = df_pelicula['release_year']
     
     return {'pelicula':pelicula, 'inversion':inversion, 'ganacia':ganancia,'retorno':retorno, 'año':anio}
 @app.get("/peliculas_recomendadas/{pelicula}")
